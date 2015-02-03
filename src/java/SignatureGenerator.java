cc79fda23490910dbcf23780738e7077725a841e
olexande <olexande@pisem.net>
2015-02-03 23:09:03 +0300

    Work at Signatures Code page fix

--- src/java/SignatureGenerator.java
+++ src/java/SignatureGenerator.java
@@ -36,26 +36,93 @@ public class SignatureGenerator extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+//        request.setCharacterEncoding("Windows-1251");
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter pout = response.getWriter()) {
             
             // Установка полученных от пользователя параметров ...
             Sotrudnik aSotrudnik = new Sotrudnik();
             
+            /*
+            
+            http://www.javaportal.ru/java/articles/ruschars/ruschars.html#servlet
+            
+            Проблема с перекодировкой полученного от пользователя.
+            
+            //////////////////////////
+            О методе перекодировки символов
+
+            Этот метод многие используют неправильно, наверное, не совсем понимая его суть и ограничения.
+            Он предназначен для восстановления верных кодов букв, если они были неверно проинтерпретированы.
+            Суть метода проста: из полученных неверных символов, используя соответствующую кодовую страницу,
+            восстанавливается исходный массив байтов.
+            Затем из этого массива байтов, используя уже корректную страницу, получаются нормальные кода символов. Пример:
+
+    String res = new String( src.getBytes("ISO-8859-1"), "Cp1251" );
+
+            Проблем в использовании этого приёма может быть несколько.
+            Например, для восстановления используется неверная страница, или же она может измениться в некоторых ситуациях.
+            Другая проблема может быть в том, что некоторые страницы выполняют неоднозначное преобразование byte <-> char.
+            Смотрите, например, описание ошибки за номером 4296969.
+
+            Поэтому пользоваться этим методом стоит только в самом крайнем случае, когда уже ничто другое не помогает,
+            и Вы чётко себе представляете, где именно происходит неверное преобразование символов. 
+            //////////////////////////
+            
+            
+            Ииное пока не получилось. предложения выше не помогли. Хотя, может и "не все варианты перепробовал перепробовал"...
+            У меня чуть другая "пара". 
+            
+            Ранее хватало 2-х строк:
             String login  = request.getParameter("login");
             aSotrudnik.setLogin(login);
+            
+            Может, 2 строки и избыточно, но что-то помешало все "слепить". Хотя, так - нагляднее ... 
+            
+            Уже "нормально" получается:
+            String login  = request.getParameter("login");
+            String res = new String( login.getBytes("ISO-8859-1"), "UTF-8" );
+            aSotrudnik.setLogin(res);
+            
+            Так и просится "метод-перекодировщик, что-бы убрать эту копи-пасту" ...
+            */
+            
+            
+            
+            // Получаю login
+            String login  = request.getParameter("login");
+            String res = new String( login.getBytes("ISO-8859-1"), "UTF-8" );
+            aSotrudnik.setLogin(res);
+            
+             // Получаю 
             String fullName  = request.getParameter("fullName");
-            aSotrudnik.setFullName(fullName);
+            res = new String( fullName.getBytes("ISO-8859-1"), "UTF-8" );
+            aSotrudnik.setFullName(res);
+            
+             // Получаю posada
             String posada = request.getParameter("posada");
-            aSotrudnik.setPosada(posada);
+            res = new String( posada.getBytes("ISO-8859-1"), "UTF-8" );
+            aSotrudnik.setPosada(res);
+            
+             // Получаю city
             String city   = request.getParameter("city");
-            aSotrudnik.setCity(city);
+            res = new String( city.getBytes("ISO-8859-1"), "UTF-8" );
+            aSotrudnik.setCity(res);
+            
+             // Получаю point
             String point  = request.getParameter("point");
-            aSotrudnik.setPoint(point);
+            res = new String( point.getBytes("ISO-8859-1"), "UTF-8" );
+            aSotrudnik.setPoint(res);
+            
+             // Получаю pfone
             String pfone  = request.getParameter("pfone");
-            aSotrudnik.setPfone(pfone);
+            res = new String( pfone.getBytes("ISO-8859-1"), "UTF-8" );
+            aSotrudnik.setPfone(res);
+            
+             // Получаю region
             String region = request.getParameter("Region");
-            aSotrudnik.setRegion(region);
+            res = new String( region.getBytes("ISO-8859-1"), "UTF-8" );
+            aSotrudnik.setRegion(res);
             
             List listProp = new LinkedList();      
         
